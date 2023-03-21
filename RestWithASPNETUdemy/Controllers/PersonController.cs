8667f6c1f88ec836bcdaf20772fc69f6c5f27c66
Robsson Allan <robsson.allan@gmail.com>
2023-03-20 21:35:33 -0300

    Feat: Criação da class Person

---
+++ RestWithASPNETUdemy/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPET.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PersonController : ControllerBase
+{
+
+    private readonly ILogger<PersonController> _logger;
+
+    public PersonController(ILogger<PersonController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("sum/{firstNumber}/{secondNumber}")]
+    public IActionResult Sum(string firstNumber, string secondNumber)
+    {
+        
+        return BadRequest("Invalid input");
+    }
+
+    
+
+}
